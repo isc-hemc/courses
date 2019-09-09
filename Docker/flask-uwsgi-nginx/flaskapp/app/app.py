6ce10b36263b486a1d223c04deb8e215159ab452
davidmartinez <davestringm@gmail.com>
2019-09-08 19:43:02 -0500

    Add README.md file.

--- Docker/flask-uwsgi-nginx/flaskapp/app/app.py
+++ Docker/flask-uwsgi-nginx/flaskapp/app/app.py
@@ -1,13 +1,27 @@
-from flask import Flask
+"""Flask server.
 
+Simple flask application with a single endpoint.
+
+Attributes
+----------
+app : Flask
+    Flask instance to run our service.
+
+"""
+
+
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def hello() -> str:
+    """Root endpoint.
+
+    Returns
+    -------
+    HttpResponse
+
+    """
     return "<h1>Hello, world!</h1>"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
