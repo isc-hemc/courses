88c8d6d937b809cf3ca62b14e32e7dbe1929f5d4
davidmartinez <davestringm@gmail.com>
2019-10-16 02:55:29 -0500

    Remove coding interview algorithms for the moment.

--- js-coding-interview-algorithms/palindrome/index.js
+++
@@ -1,7 +0,0 @@
-const isPalidrome = str => {
-    str = str.toLowerCase().replace(/\s/g, '');
-    let len = str.length;
-    return str.split('').every((char, i) => {
-        return char === str[len - i - 1];
-    });
-}
