ee0e829aa3cead851303efd2e1ad30324c5ee409
davidmartinez <davestringm@gmail.com>
2019-10-18 00:03:15 -0500

    Add initial project template for all examples.

---
+++ ReactJs/app-template/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    rules: {
+        "sort-imports": ["error", {
+            "ignoreCase": false,
+            "ignoreDeclarationSort": false,
+            "ignoreMemberSort": false,
+            "memberSyntaxSortOrder": ["none", "single", "multiple", "all"]
+        }]
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    }
+};
---
+++ ReactJs/app-template/src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css';
+import React from 'react';
+
+const App: React.FC = () => {
+    return <div></div>;
+};
+
+export default App;
