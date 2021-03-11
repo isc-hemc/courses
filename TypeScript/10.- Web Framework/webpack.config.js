fae16f2dc17e4f5f24c7a72e499255a31085d828
David Martinez <davestringm@gmail.com>
2021-03-11 01:06:10 -0600

    use webpack plugins and start a development environment configuration

--- TypeScript/10.- Web Framework/webpack.config.js
+++ TypeScript/10.- Web Framework/webpack.config.js
@@ -1,9 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
-  mode: 'none',
+  devtool: 'inline-source-map',
+  entry: path.resolve(process.cwd(), 'src/index.ts'),
+  mode: 'development',
   module: {
     rules: [
       {
@@ -15,9 +19,20 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(process.cwd(), 'dist'),
   },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(process.cwd(), 'src/index.html'),
+    }),
+  ],
   resolve: {
     extensions: ['.ts', '.js'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
   },
 };
