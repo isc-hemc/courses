0c9b27ce5f81ec8d377125654c4fd5981bc1e123
David Martinez <davestringm@gmail.com>
2021-03-10 23:46:35 -0600

    use webpack as bundler and configure it to transpile typescript

---
+++ TypeScript/10.- Web Framework/webpack.config.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/',
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
